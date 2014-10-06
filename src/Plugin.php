e824d60865c1dcfe62a3ad48f34d65b4e2f7bdc4
jeremy.derusse <jeremy.derusse@sensiolabs.com>
2014-10-06 21:57:36 +0200

    Initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Jderusse;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        echo 'ici';
+        die;
+    }
+}
