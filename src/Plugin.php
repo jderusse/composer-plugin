5c35ca9474d2520d3fe66caf2df30bd52c27967a
Jérémy Derussé <jeremy.derusse@sensiolabs.com>
2015-02-25 23:55:01 +0100

    Take last updates into account

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,10 +5,10 @@ namespace Jderusse;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Plugin\PluginCommandInterface;
+use Composer\Plugin\CommandsProviderInterface;
 use Jderusse\Console\Command\GreetCommand;
 
-class Plugin implements PluginCommandInterface
+class Plugin implements CommandsProviderInterface, PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
